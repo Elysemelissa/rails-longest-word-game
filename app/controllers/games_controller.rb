8974843d7103bc67479158f6492fc5d4691d21d4
Elyse <elyse-groenendaal@outlook.com>
2021-09-16 17:54:56 +0900

    fixed backend game code, added basic styling, added bootstrap

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ("a".."z").to_a.sample(9)
+  end
+
+  def score
+    session[:score] = 0
+    @word = params[:word].upcase.chars
+    @letters = params[:letters].upcase.scan /\w/
+    valid(@word)
+    @check = existing_word?(@word)
+    if @check 
+      session[:score] += @word.length
+    end
+  end
+
+  def valid(word)
+    @valid = @word.all? { |l| @letters.include?(l) }
+    @word = @word.join
+    @letters = @letters.join
+  end
+
+  private
+  # API
+  def existing_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
